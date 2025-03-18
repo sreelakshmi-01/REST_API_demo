8cf6f026ebb2343782e39afe8b8003e91e04491e
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-03-18 19:59:58 +0530

    Electronic fetch by id edited

--- app1/urls.py
+++ app1/urls.py
@@ -4,5 +4,5 @@ from app1 import views
 urlpatterns = [
     path('all/', views.electronics_all),
     path('add', views.add_product),
-    path('electronics/<int:id>/', views.electronics_one),
+    path('electronics/<int:electronic_id>/', views.electronics_one),
 ]
